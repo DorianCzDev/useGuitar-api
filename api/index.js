f424eccac549d37a36cab279a01fb43a16469b48
Dorian Czupryniak <dorian.czupryniak@gmail.com>
2024-08-29 17:33:12 +0200

    prepare to deploy

--- api/index.js
+++ api/index.js
@@ -12,12 +12,12 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
-const { addCookieAccessHeaders } = require("./middleware/headers");
+const { addCookieAccessHeaders } = require("../middleware/headers");
 const cors = require("cors");
 
 const morgan = require("morgan");
 
-const connectDB = require("./db/connect");
+const connectDB = require("../db/connect");
 
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -25,16 +25,16 @@ app.use(cookieParser(process.env.JWT_SECRET));
 app.use(cors({ origin: true, credentials: true }));
 app.use(addCookieAccessHeaders);
 //routers
-const productRouter = require("./routes/productRoutes");
-const userRouter = require("./routes/userRoutes");
-const authRouter = require("./routes/authRoutes");
-const reviewRouter = require("./routes/reviewRoutes");
-const orderRouter = require("./routes/orderRoutes");
-const deliveryRouter = require("./routes/deliveryRoutes");
+const productRouter = require("../routes/productRoutes");
+const userRouter = require("../routes/userRoutes");
+const authRouter = require("../routes/authRoutes");
+const reviewRouter = require("../routes/reviewRoutes");
+const orderRouter = require("../routes/orderRoutes");
+const deliveryRouter = require("../routes/deliveryRoutes");
 
 //error middlewares
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("../middleware/not-found");
+const errorHandlerMiddleware = require("../middleware/error-handler");
 
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
